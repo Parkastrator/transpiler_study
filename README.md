7fd7a177c675f99584aebbc8917be10fb214481f
bureaucratastic@live.com <bureaucratastic@live.com>
2026-02-03 18:05:06 -0500

    I hate everything

---
+++ README.md
@@ -0,0 +1,34 @@
+# Elin.PluginTemplate
+
+## Consumption
+
+![nuget](https://img.shields.io/nuget/v/transpiler_study)
+
+![.NET SDK 10.0.x](https://img.shields.io/badge/SDK%2010-green?logoColor=blue&label=dotnet&labelColor=blue&link=https%3A%2F%2Fdotnet.microsoft.com%2Fen-us%2Fdownload%2Fdotnet%2F10.0)
+
+Install from NuGet, using NPM or dotnet CLI:
+```ps
+dotnet new install transpiler_study
+```
+
+## Create Project
+
+Use JetBrains Rider or Visual Studio to create a new project from template. Fill in the necessary template information in the advanced settings, or if you really want to use VSC:
+```
+dotnet new elinplugin -n MyNewMod --Guid "unique.mod.id" --ModName "My New Awesome Mod"
+```
+
+## Build
+
+The environment variable `ElinGamePath` must be set to the root path of the Elin installation. 
+```
+ElinGamePath/
+├─ BepInEx/
+│  ├─ core/
+│  │  ├─ *.dll
+├─ Elin_Data/
+│  ├─ Managed/
+│  │  ├─ *.dll
+```
+
+Anything put inside `package/` folder will be copied to the output folder, which is `ElinGamePath/Package/Mod_{ModName}/`.
---
+++ TranspilerPatch.cs
@@ -0,0 +1,67 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using NPOI.SS.Formula.Functions;
+using transpiler_study;
+using Code = HarmonyLib.Code;
+
+namespace transpiler_study;
+
+[BepInPlugin(ModInfo.Guid, ModInfo.Name, ModInfo.Version)]
+internal class TranspilerMod : BaseUnityPlugin
+{
+    internal static TranspilerMod? Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+        var harmony = new Harmony(ModInfo.Guid);
+        harmony.PatchAll();
+    }
+    
+
+   
+    [HarmonyPatch]
+    class Patchy
+    {
+
+        private static long really_hurt_them(long damage)
+        {
+            Msg.Nerun("This attack was going to do " + damage.ToString() +" damage.");
+            Msg.Nerun("MUWAHAHAHAHA");
+            return 99999999999;
+        }
+
+        private static void say_damage(long damage)
+        {
+            Msg.Nerun(damage.ToString());
+        }
+
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(Card), nameof(Card.DamageHP), new Type[] { typeof(long), typeof(int), typeof(int), typeof(AttackSource), typeof(Card), typeof(bool), typeof(Thing), typeof(Chara) })]
+        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return new CodeMatcher(instructions)
+                .MatchForward(true, //
+                    new CodeMatch(OpCodes.Stloc_3)) //looks for first instance of this opcode being called
+                //.Advance(-1) we're not using this but this is for like, moving the current position around. You could use Advance(-1) to start inserting code RIGHT BEFORE the point where your codematcher has stopped.
+                .ThrowIfInvalid("Did not find it")//This just throws an error message if our CodeMatcher sequences does not show up (it won't)
+                .InsertAndAdvance(
+                    new CodeInstruction(OpCodes.Ldarg_1),//This pushes the value in argument one (this is damage) onto the stack
+                    Transpilers.EmitDelegate(really_hurt_them),//This calls the function above
+                    new CodeInstruction(OpCodes.Starg_S, 1)//This store the value returned from really_hurt_them into the place where argument one (again, damage) is.
+                    )
+                        
+                .InstructionEnumeration();
+        }
+     
+    }
+    
+
+}
